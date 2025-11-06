5e24c344306b25d4a249292f01972d4201b55a95
ElizaKorzhovska <ipz252_kev@student.ztu.edu.ua>
2025-11-06 19:42:43 +0200

    Обчислення виразів x, y, z, r

--- LinearExpressions1/Program.cs
+++ LinearExpressions1/Program.cs
@@ -11,6 +11,11 @@ Console.WriteLine("Введіть значення для c: ");
 c = double.Parse(Console.ReadLine());
 Console.WriteLine("Введіть значення для d: ");
 d = double.Parse(Console.ReadLine());
+x = ((a + 2 * b - c + d) / (c * d)) + ((a + b) / (c - d)) - (Math.Pow(a, 2) / Math.Pow(b, 2));
+y = (5 * (a + b) * (c - d)) / (0.5 * c) + Math.Pow(d, 2) * ((Math.Pow(a, 2) - Math.Pow(b, 2)) / (b - a));
+z = (((Math.Pow(x, 2) - 2 * x) * (Math.Pow(x, 2) - 2 * x) * (Math.Pow(x, 2) - 2 * x)) - 4 * (Math.Pow(x, 4) + 1)) * (1 - b) / (5 * a + 3 * b);
+r = ((0.5 * a + 0.75 * b - 7.0 / 5.0) / (3 * c + 1)) + (1 / (a - c));
+
 
 
 
