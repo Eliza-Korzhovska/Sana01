e519ebef9e4ff02270313f0027559482f72da482
ElizaKorzhovska <ipz252_kev@student.ztu.edu.ua>
2025-11-06 19:39:03 +0200

    Введення значень змінних

--- LinearExpressions1/Program.cs
+++ LinearExpressions1/Program.cs
@@ -2,6 +2,15 @@
 Console.InputEncoding = System.Text.Encoding.Unicode;
 double a, b, c, d;
 double x, y, z, r;
+Console.WriteLine("Введіть 4 числа a, b, c, d");
+Console.WriteLine("Введіть значення для a: ");
+a = double.Parse(Console.ReadLine());
+Console.WriteLine("Введіть значення для b: ");
+b = double.Parse(Console.ReadLine());
+Console.WriteLine("Введіть значення для c: ");
+c = double.Parse(Console.ReadLine());
+Console.WriteLine("Введіть значення для d: ");
+d = double.Parse(Console.ReadLine());
 
 
 
