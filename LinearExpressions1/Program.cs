245c76b03d7bd916e19629a73812fb259082847d
ElizaKorzhovska <ipz252_kev@student.ztu.edu.ua>
2025-11-06 19:44:04 +0200

    Виведення результатів обчислень

--- LinearExpressions1/Program.cs
+++ LinearExpressions1/Program.cs
@@ -15,7 +15,12 @@ x = ((a + 2 * b - c + d) / (c * d)) + ((a + b) / (c - d)) - (Math.Pow(a, 2) / Ma
 y = (5 * (a + b) * (c - d)) / (0.5 * c) + Math.Pow(d, 2) * ((Math.Pow(a, 2) - Math.Pow(b, 2)) / (b - a));
 z = (((Math.Pow(x, 2) - 2 * x) * (Math.Pow(x, 2) - 2 * x) * (Math.Pow(x, 2) - 2 * x)) - 4 * (Math.Pow(x, 4) + 1)) * (1 - b) / (5 * a + 3 * b);
 r = ((0.5 * a + 0.75 * b - 7.0 / 5.0) / (3 * c + 1)) + (1 / (a - c));
+Console.WriteLine($"x = {x}");
+Console.WriteLine($"y = {y}");
+Console.WriteLine($"z = {z}");
+Console.WriteLine($"r = {r}");
 
+0
 
 
 
